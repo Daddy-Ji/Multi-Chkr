03064abff0d97c591bebcf9c1c26c1e253fa0f89
Daddy-Ji <confidential.max.999@gmail.com>
2026-09-09 01:59:20 +0530

    Implement gate management functions in gates.py

---
+++ gates.py
@@ -0,0 +1,25 @@
+from db import *
+
+def list_categories():
+    return get_categories()
+
+def create_category(name):
+    add_category(name)
+
+def remove_category(cat_id):
+    delete_category(cat_id)
+
+def create_gate(cat_id, name, command, sites=None, extra=""):
+    add_gate(cat_id, name, command, sites, extra)
+
+def remove_gate(gate_id):
+    delete_gate(gate_id)
+
+def get_gates_for_category(cat_id):
+    return get_gates_by_category(cat_id)
+
+def get_gate_details(command):
+    return get_gate_by_command(command)
+
+def get_all_gates_info():
+    return get_all_gates()
